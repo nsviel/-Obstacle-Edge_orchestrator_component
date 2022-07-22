72200b7581bb8d37e35aa93c6cd50d888e845e28
nsviel <nathan.sanchizviel@pm.me>
2022-07-22 17:58:08 +0200

    commit

--- SOCK/sock_client_fct.py
+++ SOCK/sock_client_fct.py
@@ -14,18 +14,10 @@ def send_packet(packet):
     ip = param_hu.state_py["hubium"]["ip"]
     port = param_hu.state_py["hubium"]["sock_server_l1_port"]
     if(packet != None and param_hu.sock_client_ok):
-        #Remove network queue data
-        packet = packet[42:]
-
-        #Send raw data
         param_hu.sock_client.sendto(packet, (ip, port))
 
 def send_packet_add(ip, port, packet):
     if(packet != None and param_hu.sock_client_ok):
-        #Remove network queue data
-        packet = packet[42:]
-
-        #Send raw data
         param_hu.sock_client.sendto(packet, (ip, port))
 
 def test_velo_connection():
