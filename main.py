6c71fd6e00316debd55001afa5eca90beeef3032
nsviel <nathan.sanchizviel@pm.me>
2022-09-01 15:09:24 +0200

    main_header

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from src import loop
 from src import signal
+from src import connection
 
+import socket
 import os
 
 
+#Clear terminal
 os.system('clear')
-print("--- \033[1;34mBegin program\033[0m ---")
+
+#Header
+program = "Hubium"
+hostname = socket.gethostname()
+ip = connection.get_ip_adress()
+print("-----------------------")
+print("Program: \033[1;34m%s\033[0m"% program)
+print("IP: \033[1;34m%s\033[0m"% ip)
+print("Hostname: \033[1;34m%s\033[0m"% hostname)
+print("-----------------------")
 #-------------
 
 loop.start()
 
 #-------------
-print("--- \033[1;34mClose program\033[0m ---")
+print("-----------------------")
+print("\033[1;34mExit\033[0m")
+print("-----------------------")
