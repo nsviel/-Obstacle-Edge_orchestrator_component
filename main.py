ae073ecf16acce715c69986af5eaf5953dae1064
nsviel <nathan.sanchizviel@pm.me>
2022-09-01 15:20:01 +0200

    header

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ os.system('clear')
 program = "Hubium"
 hostname = socket.gethostname()
 ip = connection.get_ip_adress()
-print("-----------------------")
 print("Program: \033[1;34m%s\033[0m"% program)
 print("IP: \033[1;34m%s\033[0m"% ip)
-print("Hostname: \033[1;34m%s\033[0m"% hostname)
+print("-----------------------")
+os.system('hostnamectl')
 print("-----------------------")
 #-------------
 
@@ -24,5 +24,4 @@ loop.start()
 
 #-------------
 print("-----------------------")
-print("\033[1;34mExit\033[0m")
-print("-----------------------")
+print("Program \033[1;34mexit\033[0m")
