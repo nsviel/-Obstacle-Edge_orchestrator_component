393dfa63584c9c5639bb2288308593d83ade59c8
nsviel <nathan.sanchizviel@pm.me>
2022-09-14 13:37:57 +0200

    truc

--- docker/update.sh
+++ docker/update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+GREEN='\033[92m'
+printf "${GREEN}--------------------------${NC}\n"
+printf "${GREEN} Pull, build & run docker ${NC}\n"
+printf "${GREEN}--------------------------${NC}\n"
+
 cd ..
 git reset --hard HEAD
 git pull
