95d7c78f84bfcf30b5c353bebfda6882baf67819
nsviel <nathan.sanchizviel@pm.me>
2022-11-17 10:22:04 +0100

    up

--- src/prediction.py
+++ src/prediction.py
@@ -14,10 +14,10 @@ def format_prediction():
     pred = {
         "Timestamp": int(datetime.datetime.now().timestamp()),
         "Position": {
-        "Longitude": 23.84,
-        "Latitude": 8.48,
-        "Speed": 50,
-        "Direction": "ToFrance"
+            "Longitude": 23.843,
+            "Latitude": 8.485,
+            "Speed": 50,
+            "Direction": "ToFrance"
         },
         "Label": json["detections"][0]["name"],
         "Dimension":{
@@ -26,7 +26,7 @@ def format_prediction():
             "Heigth": round(json["detections"][0]["dimensions"][2], 3),
             "Direction": round(json["detections"][0]["heading"], 3)
         },
-        "Accuracy": 49
+        "Accuracy": round(json["detections"][0]["score"], 3)
     }
 
     # Save formatted prediction into state file
