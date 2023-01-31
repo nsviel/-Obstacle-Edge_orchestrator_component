f23ff15bab8879da146635ea4f537f57cd0fb275
nsviel <nathan.sanchizviel@pm.me>
2023-01-31 18:11:53 +0100

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash!
 
 #Install dependancies
-sudo apt install -y python3 python3-scapy libiperf0 
-sudo pip3 install requests paho-mqtt iperf3 pymongo
+sudo apt install -y python3 python3-scapy libiperf0
+sudo python3 -m pip install requests paho-mqtt iperf3 pymongo
