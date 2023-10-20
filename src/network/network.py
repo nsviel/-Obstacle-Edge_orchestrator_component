702b71836849443d662d52b430ecc8312fca71b7
nsviel <nathan.sanchizviel@pm.me>
2023-10-20 16:57:43 +0200

    up

--- src/network/network.py
+++ src/network/network.py
@@ -37,28 +37,49 @@ class Network(daemon.Daemon):
         parser_json.upload_file(param_edge.path_state_current + "state_network.json", param_edge.state_network)
 
     def update_perf_from_ground(self):
-        ground_state_network = https_client_get.get_state("network")
-        if(ground_state_network == None):
-            return
+        if(param_edge.state_edge["interface"]["capture"]["http_connected"] == True):
+            ground_state_network = https_client_get.get_state("network")
+            if(ground_state_network == None):
+                return
 
-        param_edge.state_network["ground_to_edge"]["timestamp"] = ground_state_network["ground_to_edge"]["timestamp"]
+            param_edge.state_network["ground_to_edge"]["timestamp"] = ground_state_network["ground_to_edge"]["timestamp"]
 
-        param_edge.state_network["ground_to_edge"]["throughput"]["value"] = ground_state_network["ground_to_edge"]["throughput"]["value"]
-        param_edge.state_network["ground_to_edge"]["throughput"]["min"] = ground_state_network["ground_to_edge"]["throughput"]["min"]
-        param_edge.state_network["ground_to_edge"]["throughput"]["max"] = ground_state_network["ground_to_edge"]["throughput"]["max"]
-        param_edge.state_network["ground_to_edge"]["throughput"]["mean"] = ground_state_network["ground_to_edge"]["throughput"]["mean"]
+            param_edge.state_network["ground_to_edge"]["throughput"]["value"] = ground_state_network["ground_to_edge"]["throughput"]["value"]
+            param_edge.state_network["ground_to_edge"]["throughput"]["min"] = ground_state_network["ground_to_edge"]["throughput"]["min"]
+            param_edge.state_network["ground_to_edge"]["throughput"]["max"] = ground_state_network["ground_to_edge"]["throughput"]["max"]
+            param_edge.state_network["ground_to_edge"]["throughput"]["mean"] = ground_state_network["ground_to_edge"]["throughput"]["mean"]
 
-        param_edge.state_network["ground_to_edge"]["reliability"]["value"] = ground_state_network["ground_to_edge"]["reliability"]["value"]
-        param_edge.state_network["ground_to_edge"]["reliability"]["min"] = ground_state_network["ground_to_edge"]["reliability"]["min"]
-        param_edge.state_network["ground_to_edge"]["reliability"]["max"] = ground_state_network["ground_to_edge"]["reliability"]["max"]
-        param_edge.state_network["ground_to_edge"]["reliability"]["mean"] = ground_state_network["ground_to_edge"]["reliability"]["mean"]
+            param_edge.state_network["ground_to_edge"]["reliability"]["value"] = ground_state_network["ground_to_edge"]["reliability"]["value"]
+            param_edge.state_network["ground_to_edge"]["reliability"]["min"] = ground_state_network["ground_to_edge"]["reliability"]["min"]
+            param_edge.state_network["ground_to_edge"]["reliability"]["max"] = ground_state_network["ground_to_edge"]["reliability"]["max"]
+            param_edge.state_network["ground_to_edge"]["reliability"]["mean"] = ground_state_network["ground_to_edge"]["reliability"]["mean"]
 
-        param_edge.state_network["ground_to_edge"]["interruption"]["value"] = ground_state_network["ground_to_edge"]["interruption"]["value"]
-        param_edge.state_network["ground_to_edge"]["interruption"]["min"] = ground_state_network["ground_to_edge"]["interruption"]["min"]
-        param_edge.state_network["ground_to_edge"]["interruption"]["max"] = ground_state_network["ground_to_edge"]["interruption"]["max"]
-        param_edge.state_network["ground_to_edge"]["interruption"]["mean"] = ground_state_network["ground_to_edge"]["interruption"]["mean"]
+            param_edge.state_network["ground_to_edge"]["interruption"]["value"] = ground_state_network["ground_to_edge"]["interruption"]["value"]
+            param_edge.state_network["ground_to_edge"]["interruption"]["min"] = ground_state_network["ground_to_edge"]["interruption"]["min"]
+            param_edge.state_network["ground_to_edge"]["interruption"]["max"] = ground_state_network["ground_to_edge"]["interruption"]["max"]
+            param_edge.state_network["ground_to_edge"]["interruption"]["mean"] = ground_state_network["ground_to_edge"]["interruption"]["mean"]
 
-        param_edge.state_network["ground_to_edge"]["latency"]["value"] = ground_state_network["ground_to_edge"]["latency"]["value"]
-        param_edge.state_network["ground_to_edge"]["latency"]["min"] = ground_state_network["ground_to_edge"]["latency"]["min"]
-        param_edge.state_network["ground_to_edge"]["latency"]["max"] = ground_state_network["ground_to_edge"]["latency"]["max"]
-        param_edge.state_network["ground_to_edge"]["latency"]["mean"] = ground_state_network["ground_to_edge"]["latency"]["mean"]
+            param_edge.state_network["ground_to_edge"]["latency"]["value"] = ground_state_network["ground_to_edge"]["latency"]["value"]
+            param_edge.state_network["ground_to_edge"]["latency"]["min"] = ground_state_network["ground_to_edge"]["latency"]["min"]
+            param_edge.state_network["ground_to_edge"]["latency"]["max"] = ground_state_network["ground_to_edge"]["latency"]["max"]
+            param_edge.state_network["ground_to_edge"]["latency"]["mean"] = ground_state_network["ground_to_edge"]["latency"]["mean"]
+        else:
+            param_edge.state_network["ground_to_edge"]["throughput"]["value"] = 0
+            param_edge.state_network["ground_to_edge"]["throughput"]["min"] = 0
+            param_edge.state_network["ground_to_edge"]["throughput"]["max"] = 0
+            param_edge.state_network["ground_to_edge"]["throughput"]["mean"] = 0
+
+            param_edge.state_network["ground_to_edge"]["reliability"]["value"] = 0
+            param_edge.state_network["ground_to_edge"]["reliability"]["min"] = 0
+            param_edge.state_network["ground_to_edge"]["reliability"]["max"] = 0
+            param_edge.state_network["ground_to_edge"]["reliability"]["mean"] = 0
+
+            param_edge.state_network["ground_to_edge"]["interruption"]["value"] = 0
+            param_edge.state_network["ground_to_edge"]["interruption"]["min"] = 0
+            param_edge.state_network["ground_to_edge"]["interruption"]["max"] = 0
+            param_edge.state_network["ground_to_edge"]["interruption"]["mean"] = 0
+
+            param_edge.state_network["ground_to_edge"]["latency"]["value"] = 0
+            param_edge.state_network["ground_to_edge"]["latency"]["min"] = 0
+            param_edge.state_network["ground_to_edge"]["latency"]["max"] = 0
+            param_edge.state_network["ground_to_edge"]["latency"]["mean"] = 0
